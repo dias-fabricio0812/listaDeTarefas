9983e3b7636baec05fc73368dea5464ff49da6b1
fcdias <dias.fabricio0812@gmail.com>
2024-03-14 00:39:29 -0300

    Lista de tarefas com html e js

---
+++ script.js
@@ -0,0 +1,49 @@
+let listElement = document.querySelector('#app ul')
+let inputElement = document.querySelector('#app input')
+let buttonElement = document.querySelector('#app button')
+
+let tarefas = []
+
+function renderTarefas(){
+    listElement.innerHTML = ''
+
+    tarefas.map((todo) => {
+        let liElement = document.createElement('li')
+        let tarefaText = document.createTextNode(todo)
+
+        let linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        let linkText = document.createTextNode('Excluir')
+        linkElement.appendChild(linkText)
+
+        let posicao = tarefas.indexOf(todo)
+
+        linkElement.setAttribute('onclick', `deletarTarefas(${posicao})`)
+
+        liElement.appendChild(tarefaText)
+        liElement.appendChild(linkElement)
+        listElement.appendChild(liElement)
+    })
+}
+
+function adicionarTarefas(){
+    if(inputElement.value === ''){
+        alert('Digite alguma tarefa!')
+        return false
+    }else{
+        let novaTarefa = inputElement.value
+
+        tarefas.push(novaTarefa)
+        inputElement.value = ''
+
+        renderTarefas()
+    }
+}
+
+buttonElement.onclick = adicionarTarefas
+
+function deletarTarefas(posicao){
+    tarefas.splice(posicao, 1)
+    renderTarefas()
+}
